4d8f512626b8316dfb3b50c883d274ae7c4ebe39
Manpatel9094 <17bmiit109@gmail.com>
2021-07-13 10:20:03 +0530

    init2

---
+++ src/app/components/student-data/student-data.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-data',
+  templateUrl: './student-data.component.html',
+  styleUrls: ['./student-data.component.css']
+})
+export class StudentDataComponent implements OnInit {
+
+  sname:string=""
+
+  constructor() { }
+
+ 
+
+  ngOnInit(): void {
+  }
+
+}
